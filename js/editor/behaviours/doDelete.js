97ed62dd30edb268c6d0630d568baf11b13af53a
Leanne Northrop <leanne.northrop@gmail.com>
2015-01-22 18:36:58 +0000

    Added basic delete

--- js/editor/behaviours/doDelete.js
+++ js/editor/behaviours/doDelete.js
@@ -2,6 +2,13 @@ define(['jquery', 'bootstrap', 'marionette'], function(Jquery, Bootstrap, Marion
   var DeleteBehavior = Backbone.Marionette.Behavior.extend({
     onDelete: function() {
       console.log("Do editor delete");
+      var view = this.view;
+      var doc = this.view.model.get("currentDocument");
+      this.listenToOnce(doc, "change:text", function() {
+        var ace = view.getRegion('editor').currentView.markdownEditor;
+        ace.setValue(doc.get("text"));
+      });
+      doc.set("text", "");
     }
   });
 
