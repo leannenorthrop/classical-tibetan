d5a0b21538ccf76958abf2ee6bd30f71b0dec2ea
Leanne Northrop <leanne.northrop@gmail.com>
2015-01-22 18:54:16 +0000

    Added basic zen mode

--- js/editor/behaviours/doToggleColumnSize.js
+++ js/editor/behaviours/doToggleColumnSize.js
@@ -1,7 +1,27 @@
-define(['jquery', 'bootstrap', 'marionette'], function(Jquery, Bootstrap, Marionette){
+define(['jquery', 'bootstrap', 'marionette'], function($, Bootstrap, Marionette){
   var OnToggleColumnSizeBehavior = Backbone.Marionette.Behavior.extend({
-    onToggleColumnSize: function() {
+    onToggleColumnSize: function(event) {
       console.log("Do editor toggle column size");
+      var col = event.col === "leftColumn" ? "left-col" : "right-col";
+      var otherCol = event.col === "leftColumn" ? "right-col" : "left-col";
+
+      if ($("#"+otherCol).width() === 0) {
+        $("#"+otherCol).animate({display: "table-cell", width: "50%"}, 5, function() {
+         $( "#" + col).animate({
+            width: "50%"
+          }, 1000, function() {
+            // Animation complete.
+          });
+        });
+      } else {
+        $("#"+otherCol).animate({display: "none", width: "0%"}, 5, function() {
+         $( "#" + col).animate({
+            width: "100%"
+          }, 1000, function() {
+            // Animation complete.
+          });
+        });
+      }
     }
   });
 
