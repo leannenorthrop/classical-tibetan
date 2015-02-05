2399c6f97f6ee39067bb943314fea0d4511ecd7a
Leanne Northrop <leanne.northrop@gmail.com>
2015-02-05 11:46:18 +0000

    Removed old test posts...again. Due to caching issues with Jekyll and json files Open Document now reads via github api.

--- js/editor/behaviours/doOpen.js
+++ js/editor/behaviours/doOpen.js
@@ -2,33 +2,34 @@ define(['jquery',
         'bootstrap',
         'marionette',
         'editor/views/openDocument',
+        'editor/models/document',
         'editor/collections/help'],
-function($, Bootstrap, Marionette, OpenDocumentView, HelpFiles){
+function($, Bootstrap, Marionette, OpenDocumentView, DocumentModel, HelpFiles){
   var OnOpenBehavior = Backbone.Marionette.Behavior.extend({
     onOpen: function(event) {
       console.log("Do editor open");
 
       this.options.view = this.view;
       this.options.model = this.view.model;
-      this.options.doc = this.view.model.get("currentDocument");
+      this.options.doc = new DocumentModel({file: 'post_index.json'});
 
       var me = this;
-      var help = new HelpFiles();
-      help.fetch({cache: false,
-        success: function(collection, response, options) {
-          var modalView = new OpenDocumentView({collection:collection, options: me.options});
-          modalView.render();
-          $("body").append(modalView.el);
-          $(modalView.elId).on('show.bs.modal', {}, modalView.onDisplay);
-          $(modalView.elId).modal({show: true,keyboard: true});
-        },
-        error: function(collection, response, options) {
-          console.log(response);
-          if (window && window.editorApp) {
-            window.editorApp.alert("Unable to retrieve file list from GitHub. Lost internet connection?<br/>(" + response + ")", "danger", "Error");
-          }
+      this.options.onSuccess = function() {
+        var model = me.options.doc;
+        var jsonObj = $.parseJSON(model.get("text"));
+        var collection = new HelpFiles(jsonObj);
+        var modalView = new OpenDocumentView({collection:collection, options: me.options});
+        modalView.render();
+        $("body").append(modalView.el);
+        $(modalView.elId).on('show.bs.modal', {}, modalView.onDisplay);
+        $(modalView.elId).modal({show: true,keyboard: true});
+      };
+      this.options.onError = function() {
+        if (window && window.editorApp) {
+          window.editorApp.alert("Unable to retrieve file list from GitHub. Lost internet connection?", "danger", "Error");
         }
-      });
+      };
+      this.options.doc.open(this.options);
     }
   });
 
