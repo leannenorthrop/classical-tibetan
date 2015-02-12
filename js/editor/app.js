8443a8eb8bdcf14a7376c079e08131a1c87e6d78
Leanne Northrop <leanne.northrop@gmail.com>
2015-02-12 22:37:48 +0000

    GHC

--- js/editor/app.js
+++ js/editor/app.js
@@ -14,7 +14,7 @@ define(["jquery",
       if (gitHubCode) {
         $.ajaxSetup({
           'beforeSend': function(xhr) {
-            xhr.setRequestHeader("Access-Control-Request-Headers", "Authorization");
+            xhr.setRequestHeader("Access-Control-Allow-Origin", "'*'");
           }
         });
         var jqxhr = $.post("https://github.com/login/oauth/access_token", {client_id:"a471bea0e772edefb748",client_secret:"42be7bfc553b8c95b1d8c94ec608fcb0366df10f",code:gitHubCode}).always(function () {
