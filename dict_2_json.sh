20568344bd8d8d8515db9136fe2cf9ac0682f488
Leanne Northrop <leanne.northrop@gmail.com>
2015-11-24 19:01:16 +0000

    Fixed a few quirks. Words.json generation now creates valid json. Fixed nav/index pages for book reviews. Fixed link to print flash cards in Vocab pages. Fixed layout of editor.

--- dict_2_json.sh
+++ dict_2_json.sh
@@ -2,4 +2,4 @@
 
 echo "[{" > words.json
 awk -F "," -v q='"' -v c=':' 'FNR > 1 { print q$1q c "{" q "en" q c q$3q "," q "rl" q c q$4q "," q "ph" q c q $8 q "," q "type" q c q $10 q "},"; }' ./_data/dictionary/all.csv >> words.json
-echo "}]" >> words.json
+echo '"":{}}]' >> words.json
